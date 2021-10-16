c54fdfb6f5e6d094fe775e8e25af503402e36b6a
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-15 21:58:26 -0300

    Fazendo exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      dogImage: '',
+    };
+
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  fetchDog() {
+    this.setState(
+      { isLoading: true },
+      async () => {
+        const dogJson = await fetch('https://dog.ceo/api/breeds/image/random');
+        const dogObj = await dogJson.json();
+        const { message: dogImageUrl } = dogObj;
+        this.setState({
+          isLoading: false,
+          dogImage: dogImageUrl,
+        });
+      },
+    );
+  }
+
+  render() {
+    const { isLoading, dogImage } = this.state;
+    return (
+      <div className="App">
+        <h1>Cachorros</h1>
         <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
+          {
+            isLoading
+              ? <span>Carregando...</span>
+              : <img className="dogImg" src={ dogImage } alt="Dog" />
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button onClick={ this.fetchDog } type="button">Próximo</button>
+      </div>
+    );
+  }
 }
 
 export default App;
